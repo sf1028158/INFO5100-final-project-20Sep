e737bae2ebfd84019e3b206dd4bf16f9c48ccc10
Erica M Yuan <59790872+sdwfym@users.noreply.github.com>
2020-12-08 10:37:41 -0800

    combine Fang's part to ui

--- src/ui/DealerScreen.java
+++ src/ui/DealerScreen.java
@@ -8,6 +8,7 @@ import service.*;
 //import ui.*; 
 import validation.*;
 
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.event.DocumentEvent;
@@ -30,7 +31,11 @@ public class DealerScreen {
     private JTextField textFieldZipCode;
     private JComboBox<String> comboBox;
     private JButton btnSearch;
-    // private ArrayList<Dealer> dealerList=new ArrayList<>();
+    
+    private JTable table;
+    int signal = 0;
+    
+    //private ArrayList<Dealer> dealerList=new ArrayList<>();
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -50,6 +55,7 @@ public class DealerScreen {
         frameAndPanel();
         dealerName();
         zipCode();
+        searchBtn();
         // mileRange(); // Fang's method
         // searchButton(); // Fang's method
     }
@@ -210,5 +216,137 @@ public class DealerScreen {
             }
         });
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private void searchBtn(){
+    	//int signal = 0;   	
+    	ArrayList<Dealer> searchList = null;
+    	SearchDealer s1 = new SearchDealer();
+    	JLabel l1 = new JLabel("Search By: ");
+    	JRadioButton c1=new JRadioButton("Zipcode");
+    	JRadioButton c2=new JRadioButton("Dealer Name");
+    	JButton c3=new JButton("Search");
+    	 l1.setBounds(10, 270, 150, 14);
+    	 c1.setBounds(190, 270, 80, 14);
+    	 c1.setBounds(290, 270, 80, 14);
+    	 c3.setBounds(10,300,100,14);
+    	ButtonGroup group = new ButtonGroup();  
+    	group.add(c1);
+    	group.add(c2);  
+    	panelLeft.add(l1);
+    	panelLeft.add(c1);
+    	panelLeft.add(c2);	
+    	panelLeft.add(c3);
+		c1.addActionListener((ActionEvent ae) ->{signal=0;});
+		
+		c1.addActionListener((ActionEvent ae) ->{signal=1;});
+		c3.addActionListener((ActionEvent ae) ->{
+	
+			if(signal==0){
+				
+				ArrayList<Dealer> searchList1=(ArrayList<Dealer>) s1.searchByZipCode(textFieldZipCode.getText());
+				try {
+					createTable(searchList1);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				setJTableColumnsWidth(table, 200);
+				}
+			if(signal==1){
+				
+				ArrayList<Dealer> searchList2=(ArrayList<Dealer>)s1.searchByName(textFieldDealerName.getText());
+			 try {
+				createTable(searchList2);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			setJTableColumnsWidth(table, 200);
+				}	
+		});	
+		
+    }
+    
+    
+    private void createTable(ArrayList<Dealer> dealerList1) throws Exception { 	
+    	
+    	 String[] columnNames = { "ID", "Name","ZipCode","Distance","City","State","Address"};
+    	 String[][] obj=null;
+    	 table=new JTable();		 
+    	 int n= dealerList1.size();    	 
+    	 for(int i=0;i<n;i++){
+    		 obj[i][0]=dealerList1.get(i).getDealerId();
+    		 obj[i][1]=dealerList1.get(i).getDealerName();
+    		 obj[i][2]=dealerList1.get(i).getDealerAddress().getZipCode();
+    		 obj[i][3]=dealerList1.get(i).getDistanceInMiles()+"";
+    		 obj[i][4]=dealerList1.get(i).getDealerAddress().getCity();
+    		 obj[i][5]=dealerList1.get(i).getDealerAddress().getState();
+    		 obj[i][6]=dealerList1.get(i).getDealerAddress().getAddressInfo();
+    	 }
+    	 TableModel tm = new DefaultTableModel(obj, columnNames); 	 
+    	 this.table.setModel(tm);
+    	
+          panelRight.add(new JScrollPane(table)); 
+    	 try {
+             if(dealerList1.isEmpty()) {
+                 JOptionPane.showMessageDialog(frame, "There is no dealer satisfied your requirement."); 
+             }
+         }
+         catch(Exception e) {
+         	e.printStackTrace();
+         }
+    	 
+    }
+
+    public static void setJTableColumnsWidth(JTable table, int tablePreferredWidth) {
+    	
+    	table.setRowHeight(30);
+    	int columncount = table.getColumnCount();
+
+        for (int i = 1; i < columncount; i++) {
+            table.getColumnModel().getColumn(i).setPreferredWidth(tablePreferredWidth);
+
+        }
+    }  
+ 
+    
+    
+    
+   private void SelectAndView() {
+	    int count=table.getSelectedRow();//��ȡ��ѡ�е��кţ���¼��
+	    int getID= Integer.parseInt((String) table.getValueAt(count, 0));
+	    JButton jbtn= new JButton("View Inventory");
+	    panelRight.add(jbtn);
+	    jbtn.setBounds(10, 630, 80, 14);
+	    jbtn.addActionListener((ActionEvent ae) ->{
+	    	
+	    	System.out.println("Dealer"+"getID"+"is selected, and view inventory.");
+	    	
+	    });
+    	
+    	
+   }    
     
 }
